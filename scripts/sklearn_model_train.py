5555f16f8bb271368c51cda4fcdba57668cc7fcb
Jim Coates <jim.coates@dominodatalab.com>
2024-11-25 06:39:08 -0800

    Pushed from Domino: https://ucb-inc-sandbox.domino-eval.com/workspace/jcoates/MLOps-Workshop?executionId=674482139db1715867aee986

--- scripts/sklearn_model_train.py
+++ scripts/sklearn_model_train.py
@@ -60,7 +60,7 @@ with mlflow.start_run():
     print('Training model...')
     #gbr = GradientBoostingRegressor(loss='squared_error',learning_rate = 0.15, n_estimators=75, criterion = 'squared_error')
     #gbr = GradientBoostingRegressor(loss='ls', learning_rate=0.15, n_estimators=75, criterion='squared_error')
-    gbr = GradientBoostingRegressor(loss='ls', learning_rate=0.15, n_estimators=75, criterion='squared_error')
+    gbr = GradientBoostingRegressor(loss='squared_error', learning_rate=0.15, n_estimators=75, criterion='squared_error')
 
     gbr.fit(X_train,y_train)
 
